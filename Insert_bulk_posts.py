37d2e99f26ed3c93e32c9586e7a852ae3c6c36cb
Omar Faruqe <riyad.omf@gmail.com>
2021-03-25 03:04:26 +0600

    requirments.txt and dotenv

--- Insert_bulk_posts.py
+++ Insert_bulk_posts.py
@@ -1,20 +1,30 @@
 """
 Automated Insertion of bulk posts from json for testing pagination feature.
+Warning: There should be at least 2 registered user as the json data contains 2 user. 
 """
-
 
 import requests
 import json
-from brevity import db
+from brevity import create_app, db
 from brevity.models import Post, load_user
+
+def scrape_json(url):
+    posts = requests.get(url, headers = { "Accept" : "application/json" }).json()
+    return posts
+
+def insert_bulk_post(posts):
+    app = create_app()
+    with app.app_context():
+        for p in posts:
+            post = Post(title = p.get("title"), content = p.get("content"), author = load_user(p.get("user_id")))
+            db.session.add(post)
+            db.session.commit()
+
+
+
+
 
 url = "https://raw.githubusercontent.com/CoreyMSchafer/code_snippets/master/Python/Flask_Blog/snippets/posts.json"
 
-posts = requests.get(url, headers = { "Accept" : "application/json" }).json()
-
-for p in posts:
-    post = Post(title = p.get("title"), content = p.get("content"), author = load_user(p.get("user_id")))
-    db.session.add(post)
-    db.session.commit()
-
-
+posts = scrape_json(url)
+insert_bulk_post(posts)
--- brevity/config.py
+++ brevity/config.py
@@ -1,12 +1,16 @@
-import os
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')                   #secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = environ.get('SECRET_KEY')                   #secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False                      #supresses warning
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = environ.get('EMAIL_USER')
+    MAIL_PASSWORD = environ.get('EMAIL_PASS')
