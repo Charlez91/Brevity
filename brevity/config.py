013e751daed7c3ac9b9820e2aa747571817e92cd
Omar Faruqe <riyad.omf@gmail.com>
2021-03-25 04:23:16 +0600

    dotenv

--- brevity/config.py
+++ brevity/config.py
@@ -1,4 +1,4 @@
-from os import environ, path
+from os import getenv, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -6,11 +6,11 @@ load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
-    SECRET_KEY = environ.get('SECRET_KEY')                   #secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = getenv('SECRET_KEY')                   #secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False                      #supresses warning
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = environ.get('EMAIL_USER')
-    MAIL_PASSWORD = environ.get('EMAIL_PASS')
+    MAIL_USERNAME = getenv('EMAIL_USER')
+    MAIL_PASSWORD = getenv('EMAIL_PASS')
