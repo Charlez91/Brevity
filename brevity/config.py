7bd44702cd2e4009f33fc0344b6cea244e287b0d
Omar Faruqe <riyad.omf@gmail.com>
2021-03-25 22:06:11 +0600

    config file update

--- brevity/config.py
+++ brevity/config.py
@@ -12,5 +12,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = getenv('MAIL_UESRNAME')
+    MAIL_USERNAME = getenv('MAIL_USERNAME')
     MAIL_PASSWORD = getenv('MAIL_PASSWORD')
